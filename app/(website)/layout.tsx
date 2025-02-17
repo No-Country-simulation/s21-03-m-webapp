ff711dcb1fe41a4ba223ff0a96df2a14a8c30504
Tomas Padilla <padillatomasagustin@gmail.com>
2025-02-17 16:06:09 +0100

    [6] Structure Components

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,18 +1,21 @@
 'use client';
 
 import Link from 'next/link';
+import { VerticalContainer, Container } from '../../components/library/structure';
 
 export default function WebsiteLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<div>
-			<nav className="flex flex-row gap-2 justify-between items-center h-[4rem] bg-red-300">
+		<>
+			<nav className="fixed w-full z-50 flex flex-row gap-2 justify-between items-center h-[4rem] bg-red-300">
 				<div className="w-[90%] m-auto h-full flex items-center justify-between">
 					<Link href={'/register'}>Registrarse</Link>
 					<Link href={'/login'}>Login</Link>
 					<Link href={'/dashboard'}>Dashboard</Link>
 				</div>
 			</nav>
-			{children}
-		</div>
+			<VerticalContainer escape={'all'}>
+				<Container className="bg-background text-foreground">{children}</Container>
+			</VerticalContainer>
+		</>
 	);
 }
--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,16 +1,16 @@
 'use client';
 
 import { useBearStore, useDogStore } from '@/store';
+import { Button } from '../../components/ui/button';
+import { useState } from 'react';
+import { Card, CardContent, CardHeader, CardTitle } from '../../components/ui/card';
+import { Input } from '../../components/ui/input';
 
 const HomePage = () => {
 	const bears = useBearStore((state) => state.bears);
 	const dogs = useDogStore((state) => state.dogs);
 
-	return (
-		<div>
-			Mesa 360 con {bears} Bears y {dogs} Dogs
-		</div>
-	);
+	return <h2>Hola</h2>;
 };
 
 export default HomePage;
