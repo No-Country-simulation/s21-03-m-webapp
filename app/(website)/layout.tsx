e0b0be1a82b503e1c367e18895e487d47267d4bc
Tomas Padilla <padillatomasagustin@gmail.com>
2025-02-17 16:31:54 +0100

    [6] Base main website

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -6,7 +6,7 @@ import { VerticalContainer, Container } from '../../components/library/structure
 export default function WebsiteLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
-			<nav className="fixed w-full z-50 flex flex-row gap-2 justify-between items-center h-[4rem] bg-red-300">
+			<nav className="w-full h-[5rem] bg-white fixed top-0 drop-shadow-sm sm:flex flex-row items-center justify-center z-30">
 				<div className="w-[90%] m-auto h-full flex items-center justify-between">
 					<Link href={'/register'}>Registrarse</Link>
 					<Link href={'/login'}>Login</Link>
