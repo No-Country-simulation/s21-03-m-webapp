2b6f4fff3153f494038a7d285099284027a9bf19
Tomas Padilla <padillatomasagustin@gmail.com>
2025-02-19 13:55:27 +0100

    develop - add User to Context, so we get the User type available

--- context/AuthenticationContext.tsx
+++ context/AuthenticationContext.tsx
@@ -3,13 +3,14 @@
 import Cookies from 'js-cookie';
 import { createContext, useContext, useState, useEffect } from 'react';
 import { useCurrentUserQuery } from '@/actions/hooks/useCurrentUser';
-import { AuthenticationContextType } from '@/types/authentication';
+import { AuthenticationContextType, User } from '@/types/authentication';
 import { COOKIE_NAME } from '../constants/app_constants';
 
 const MyAuthContext = createContext<AuthenticationContextType | null>(null);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 	const [token, setToken] = useState<string | null>(Cookies.get(COOKIE_NAME) || null);
+	const [user, setUser] = useState<User | null>(null);
 
 	const { data, isLoading, isError, refetch } = useCurrentUserQuery(token);
 
@@ -19,6 +20,16 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 			logoutUser();
 		}
 	}, [isError, token]);
+
+	useEffect(() => {
+		if (data?.token) {
+			setToken(data.token);
+			Cookies.set(COOKIE_NAME, data.token, { expires: 7 });
+		}
+		if (data?.user) {
+			setUser(data.user);
+		}
+	}, [data]);
 
 	const updateToken = (newToken: string | null) => {
 		if (!newToken) {
@@ -33,12 +44,14 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 	const logoutUser = () => {
 		Cookies.remove(COOKIE_NAME);
 		setToken(null);
+		setUser(null);
 	};
+
 	return (
 		<MyAuthContext.Provider
 			value={{
 				token,
-				user: data?.user || null,
+				user,
 				loading: isLoading,
 				updateToken,
 				logoutUser,
