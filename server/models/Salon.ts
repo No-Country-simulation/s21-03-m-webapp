99d0efaefd084dc46e4f4c75d0e6e6fc1186731e
Tolomatico <ballesty.t@gmail.com>
2025-02-21 16:24:37 -0300

    Added middleware on salones to delete in cascade

--- server/models/Salon.ts
+++ server/models/Salon.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document, Schema, Types } from "mongoose"
+import Table from "./Table"
 
 export interface ISalon extends Document{
     name:string
@@ -22,6 +23,12 @@ const SalonSchema=new Schema({
         ref:"Table"
     }
 
+})
+
+
+SalonSchema.pre("deleteOne",{ document: true, query: false },async function(next){
+    await Table.deleteMany({salonId:this._id})
+    next()
 })
 
 const Salon=mongoose.model<ISalon>("Salon",SalonSchema)
