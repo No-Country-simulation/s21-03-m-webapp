af73faaef807161f7adc0964a2434a818aaf324b
AlanProgrammer93 <alanwebprogrammer@gmail.com>
2025-02-25 11:37:19 -0300

    add getByTable - add status

--- server/controllers/order.ts
+++ server/controllers/order.ts
@@ -12,20 +12,25 @@ export const create = async (req: Request, res: Response) => {
     }
 
     if (!items || items.length === 0) {
-        return res.status(400).json({ message: "La orden debe contener al menos un producto." });
+        return res.status(400).json({ msg: "La orden debe contener al menos un producto." });
     }
 
     try {
+        const orderExist = await Order.find({ tableNumber, status: 'pending' })
+        if (orderExist) {
+            return res.status(400).json({ msg: `Existe una orden pendiente en la mesa ${tableNumber}.` });
+        }
+
         let subtotal = 0;
         for (const item of items) {
             const product = await Product.findById(item.productId);
             if (!product) {
-                return res.status(404).json({ message: `Producto con ID ${item.productId} no encontrado` });
+                return res.status(404).json({ msg: `Producto con ID ${item.productId} no encontrado` });
             }
             subtotal += product.price * item.quantity;
             item.price = product.price
         }
-        
+
         const discountPer = discountPercentage ? (subtotal * discountPercentage) / 100 : 0;
         const total = subtotal - (discount || 0) - discountPer;
 
@@ -72,41 +77,41 @@ export const edit = async (req: Request, res: Response) => {
     const { id } = req.params
     const { tableNumber, people, items, discount } = req.body;
 
-    const order = await Order.findById(id)
-    if (!order) {
-        return res.status(404).json({
-            msg: "No existe la orden."
-        })
-    }
-
-    if (order.ownerId?.toString() !== req.ownerId?.toString()) {
-        return res.status(403).json({
-            msg: "No tiene permiso para editar la orden.",
-        });
-    }
-
-    if (!tableNumber?.trim() || !people) {
-        return res.status(400).json({
-            msg: 'Los campos numero de mesa y cantidad de personas son obligatorios.'
-        });
-    }
-
-    if (!items || items.length === 0) {
-        return res.status(400).json({ message: "La orden debe contener al menos un producto." });
-    }
-
-    let subtotal = 0;
-    for (const item of items) {
-        const product = await Product.findById(item.productId);
-        if (!product) {
-            return res.status(404).json({ message: `Producto con ID ${item.productId} no encontrado` });
-        }
-        subtotal += product.price * item.quantity;
-    }
-
-    const total = subtotal - (discount || 0);
-
     try {
+        const order = await Order.findOne({ _id: id, status: 'pending' })
+        if (!order) {
+            return res.status(404).json({
+                msg: "No existe la orden o ya no es posible editar."
+            })
+        }
+
+        if (order.ownerId?.toString() !== req.ownerId?.toString()) {
+            return res.status(403).json({
+                msg: "No tiene permiso para editar la orden.",
+            });
+        }
+
+        if (!tableNumber?.trim() || !people) {
+            return res.status(400).json({
+                msg: 'Los campos numero de mesa y cantidad de personas son obligatorios.'
+            });
+        }
+
+        if (!items || items.length === 0) {
+            return res.status(400).json({ msg: "La orden debe contener al menos un producto." });
+        }
+
+        let subtotal = 0;
+        for (const item of items) {
+            const product = await Product.findById(item.productId);
+            if (!product) {
+                return res.status(404).json({ msg: `Producto con ID ${item.productId} no encontrado` });
+            }
+            subtotal += product.price * item.quantity;
+        }
+
+        const total = subtotal - (discount || 0);
+
         order.tableNumber = tableNumber
         order.people = people
         order.items = items
@@ -119,6 +124,63 @@ export const edit = async (req: Request, res: Response) => {
         return res.status(200).json({
             msg: 'Orden Editada Correctamente.',
             order
+        });
+    } catch (error) {
+        return res.status(500).json({
+            msg: 'Ocurrio un problema en el servidor.'
+        });
+    }
+}
+
+export const updateStatus = async (req: Request, res: Response) => {
+    const { id, status } = req.body;
+
+    const order = await Order.findById(id)
+    if (!order) {
+        return res.status(404).json({
+            msg: "No existe la orden."
+        })
+    }
+    try {
+        order.status = status
+        await order.save();
+
+        return res.status(200).json({
+            msg: 'Estado Actualizado Correctamente.'
+        });
+    } catch (error) {
+        return res.status(500).json({
+            msg: 'Ocurrio un problema en el servidor.'
+        });
+    }
+}
+
+/* export const getAllStatusPending = async (req: Request, res: Response) => {
+    try {
+        const orders = await Order.find({ ownerId: req.ownerId, status: 'pending' })
+            .populate("items.productId", "name")
+            .select("-__v -createdAt -updatedAt -ownerId -items._id ")
+
+        return res.status(200).json({
+            orders: orders
+        });
+    } catch (error) {
+        return res.status(500).json({
+            msg: 'Ocurrio un problema en el servidor.'
+        });
+    }
+} */
+
+export const getOrderByTable = async (req: Request, res: Response) => {
+    try {
+        const { tableNumber } = req.params
+
+        const order = await Order.findOne({ ownerId: req.ownerId, tableNumber: tableNumber, status: 'pending' })
+            .populate("items.productId", "name")
+            .select("-__v -createdAt -updatedAt -ownerId -items._id ")
+
+        return res.status(200).json({
+            order: order
         });
     } catch (error) {
         return res.status(500).json({
--- server/routes/order.ts
+++ server/routes/order.ts
@@ -1,12 +1,14 @@
 import express from "express";
 
-const { create, getAll, edit, remove } = require('../controllers/order')
+const { create, getAll, edit, remove, updateStatus, getOrderByTable } = require('../controllers/order')
 const { authCheck } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/', authCheck, create);
 router.get('/', authCheck, getAll);
+router.put('/update-status', authCheck, updateStatus);
+router.get('/get-order/:tableNumber', authCheck, getOrderByTable);
 router.put("/:id", authCheck, edit)
 router.delete("/:id", authCheck, remove)
 
