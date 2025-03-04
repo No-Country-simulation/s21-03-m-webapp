38ae22bb808a42b4e53fed7fbadcf1e5325bde03
Tomas Padilla <padillatomasagustin@gmail.com>
2025-03-04 16:35:00 +0100

    Fix - React Query with keys

--- actions/hooks/members/useMembers.ts
+++ actions/hooks/members/useMembers.ts
@@ -1,9 +1,11 @@
 import { getMembers } from '@/actions/member';
 import { useQuery } from '@tanstack/react-query';
+import { useAuth } from '../../../context/AuthenticationContext';
 
 export const useMembers = () => {
+	const { user } = useAuth();
 	return useQuery({
-		queryKey: ['members'],
+		queryKey: ['members', user?.ownerId],
 		queryFn: () => {
 			return getMembers();
 		},
