e6a36983604d5d47ad2547317002fcf78fb04070
Tolomatico <ballesty.t@gmail.com>
2025-03-05 12:17:48 -0300

    Fix some cosas on members

--- app/(protected)/members/_components/MemberCreateModal.tsx
+++ app/(protected)/members/_components/MemberCreateModal.tsx
@@ -10,26 +10,29 @@ import {
 
 import { MemberForm } from './MemberForm';
 import { useState } from 'react';
-
 export function MemberCreateModal() {
 	const [open, setOpen] = useState(false);
+  
 	return (
-		<Dialog open={open} onOpenChange={setOpen}>
-			<DialogTrigger asChild className="absolute right-5 bottom-5 mb-5">
-				<Button
-					className="text-6xl bg-chart-1 text-white  p-12 rounded-[50%] cursor-pointer hover:bg-white hover:text-red-400"
-					variant="outline"
-				>
-					+
-				</Button>
-			</DialogTrigger>
-			<DialogContent className="sm:max-w-[425px]">
-				<DialogHeader>
-					<DialogTitle>Crear Miembro</DialogTitle>
-					<DialogDescription>Agrega un nuevo miembro a tu staff</DialogDescription>
-				</DialogHeader>
-				<MemberForm setOpen={setOpen} />
-			</DialogContent>
-		</Dialog>
+	  <Dialog open={open} onOpenChange={setOpen}>
+		<DialogTrigger asChild>
+		  <Button
+			className={`text-6xl bg-chart-1 text-white p-12 rounded-full cursor-pointer hover:bg-white hover:text-red-400 transition-opacity duration-200 ${
+			  open ? "opacity-0 pointer-events-none" : "opacity-100"
+			}`}
+			variant="outline"
+		  >
+			+
+		  </Button>
+		</DialogTrigger>
+		<DialogContent className="sm:max-w-[425px]">
+		  <DialogHeader>
+			<DialogTitle>Crear Miembro</DialogTitle>
+			<DialogDescription>Agrega un nuevo miembro a tu staff</DialogDescription>
+		  </DialogHeader>
+		  <MemberForm setOpen={setOpen} />
+		</DialogContent>
+	  </Dialog>
 	);
-}
+  }
+  
