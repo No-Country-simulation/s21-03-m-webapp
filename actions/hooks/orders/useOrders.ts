0af2016558bdb2c82eeff0dd19b78ad8d67de9a7
Tomas Padilla <padillatomasagustin@gmail.com>
2025-03-09 20:18:45 +0100

    [48] Merge develop plus fixes

--- actions/hooks/orders/useOrders.ts
+++ actions/hooks/orders/useOrders.ts
@@ -2,12 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import { getOrders } from '../../orders';
 import { useAuth } from '@/context/AuthenticationContext';
 
-
 export const useOrders = () => {
 	const { user } = useAuth();
 	return useQuery({
-
-		queryKey: ['orders',user?.ownerId],
+		queryKey: ['orders', user?.ownerId],
 		queryFn: getOrders,
 		retry: 1,
 		staleTime: 1000 * 60 * 5,
--- context/VentasContext.tsx
+++ context/VentasContext.tsx
@@ -1,83 +1,82 @@
-import { OrderCompleteResponse } from "@/types/orders";
-import { createContext, useMemo, useState, useEffect } from "react";
+import { OrderCompleteResponse } from '@/types/orders';
+import { createContext, useMemo, useState, useEffect } from 'react';
 
 export type Options = {
-    id: number;
-    label: string;
+	id: number;
+	label: string;
 };
 
-interface InitialValues{
-    day: number,
-    month: number,
-    year: number,
+interface InitialValues {
+	day: number;
+	month: number;
+	year: number;
 }
 
 interface ContextType {
-    initialValues:InitialValues
-    sortedOrders: OrderCompleteResponse[]
-    orders: OrderCompleteResponse[];
-    setOrders: React.Dispatch<React.SetStateAction<OrderCompleteResponse[]>>;
-    setSelectedDay: React.Dispatch<React.SetStateAction<number>>;
-    setSelectedMonth: React.Dispatch<React.SetStateAction<number>>;
-    setSelectedYear: React.Dispatch<React.SetStateAction<number>>;
-    selectedDay: number;
-    selectedMonth: number;
-    selectedYear: number;
-    filteredOrders: OrderCompleteResponse[];
+	initialValues: InitialValues;
+	sortedOrders: OrderCompleteResponse[];
+	orders: OrderCompleteResponse[];
+	setOrders: React.Dispatch<React.SetStateAction<OrderCompleteResponse[]>>;
+	setSelectedDay: React.Dispatch<React.SetStateAction<number>>;
+	setSelectedMonth: React.Dispatch<React.SetStateAction<number>>;
+	setSelectedYear: React.Dispatch<React.SetStateAction<number>>;
+	selectedDay: number;
+	selectedMonth: number;
+	selectedYear: number;
+	filteredOrders: OrderCompleteResponse[];
 }
 
 export const VentasContext = createContext<ContextType | undefined>(undefined);
 
 export const VentasProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const date = new Date();
-    const initialValues = {
-        day: date.getDate(),
-        month: date.getMonth() + 1,
-        year: date.getFullYear(),
-    };
+	const date = new Date();
+	const initialValues = {
+		day: date.getDate(),
+		month: date.getMonth() + 1,
+		year: date.getFullYear(),
+	};
 
-    const [selectedDay, setSelectedDay] = useState<number>(initialValues.day);
-    const [selectedMonth, setSelectedMonth] = useState<number>(initialValues.month);
-    const [selectedYear, setSelectedYear] = useState<number>(initialValues.year);
-    const [orders, setOrders] = useState<OrderCompleteResponse[]>([]);
+	const [selectedDay, setSelectedDay] = useState<number>(initialValues.day);
+	const [selectedMonth, setSelectedMonth] = useState<number>(initialValues.month);
+	const [selectedYear, setSelectedYear] = useState<number>(initialValues.year);
+	const [orders, setOrders] = useState<OrderCompleteResponse[]>([]);
 
-    useEffect(() => {
-        console.log("Fecha seleccionada:", { selectedDay, selectedMonth, selectedYear });
-    }, [selectedDay, selectedMonth, selectedYear]);
+	useEffect(() => {
+		console.log('Fecha seleccionada:', { selectedDay, selectedMonth, selectedYear });
+	}, [selectedDay, selectedMonth, selectedYear]);
 
-    const filteredOrders = useMemo(() => {
-        return orders
-            .filter((order) => {
-                const orderDate = new Date(order.createdAt);
-                return (
-                    orderDate.getDate() === selectedDay &&
-                    orderDate.getMonth() + 1 === selectedMonth &&
-                    orderDate.getFullYear() === selectedYear
-                );
-            })
-            .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()); // Orden descendente
-    }, [orders, selectedDay, selectedMonth, selectedYear]);
-    const sortedOrders = useMemo(() => {
-
-        return filteredOrders.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
-    }, [filteredOrders]);
-    return (
-        <VentasContext.Provider
-            value={{
-                initialValues,
-                sortedOrders,
-                orders,
-                setOrders,
-                selectedDay,
-                selectedMonth,
-                selectedYear,
-                setSelectedDay,
-                setSelectedMonth,
-                setSelectedYear,
-                filteredOrders,
-            }}
-        >
-            {children}
-        </VentasContext.Provider>
-    );
+	const filteredOrders = useMemo(() => {
+		return orders
+			.filter((order) => {
+				const orderDate = new Date(order.createdAt);
+				return (
+					orderDate.getDate() === selectedDay &&
+					orderDate.getMonth() + 1 === selectedMonth &&
+					orderDate.getFullYear() === selectedYear
+				);
+			})
+			.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()); // Orden descendente
+	}, [orders, selectedDay, selectedMonth, selectedYear]);
+	const sortedOrders = useMemo(() => {
+		return filteredOrders.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+	}, [filteredOrders]);
+	return (
+		<VentasContext.Provider
+			value={{
+				initialValues,
+				sortedOrders,
+				orders,
+				setOrders,
+				selectedDay,
+				selectedMonth,
+				selectedYear,
+				setSelectedDay,
+				setSelectedMonth,
+				setSelectedYear,
+				filteredOrders,
+			}}
+		>
+			{children}
+		</VentasContext.Provider>
+	);
 };
